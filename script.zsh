7119617cf9598734eac8ad9b0155b3b6787e921b
samlyme <sam@samly.me>
2025-09-25 15:27:06 -0700

    init

---
+++ script.zsh
@@ -0,0 +1,5 @@
+
+hidutil property --set '{"CapsLockDelayOverride":0}'
+
+sudo mkdir -p /Library/Preferences/FeatureFlags/Domain
+sudo /usr/libexec/PlistBuddy -c "Add 'redesigned_text_cursor:Enabled' bool false" /Library/Preferences/FeatureFlags/Domain/UIKit.plistó
