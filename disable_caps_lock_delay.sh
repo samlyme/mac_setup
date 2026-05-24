ee764607166ef544229b56fa2f1956fbdd2264bd
Sam Ly <sam@samly.me>
2026-05-23 22:08:27 -0700

    Update disable_caps_lock_delay.sh

--- disable_caps_lock_delay.sh
+++ disable_caps_lock_delay.sh
@@ -1,2 +1,29 @@
+mkdir -p ~/Library/LaunchAgents/
+echo "
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN"
+ "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>Label</key>
+    <string>com.user.capsdelayfix</string>
 
-hidutil property --set '{"CapsLockDelayOverride":0}'
+    <key>ProgramArguments</key>
+    <array>
+        <string>/usr/bin/hidutil</string>
+        <string>property</string>
+        <string>--set</string>
+        <string>{"CapsLockDelayOverride":10}</string>
+    </array>
+
+    <key>RunAtLoad</key>
+    <true/>
+
+    <key>StandardOutPath</key>
+    <string>/dev/null</string>
+
+    <key>StandardErrorPath</key>
+    <string>/dev/null</string>
+</dict>
+</plist>
+"  > ~/Library/LaunchAgents/com.user.capsdelayfix.plist
